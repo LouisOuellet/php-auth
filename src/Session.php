784be3ff0fa4646438d3062f174dc64d34a6acd4
Louis Ouellet <louis_ouellet@hotmail.com>
2022-11-17 14:17:54 -0500

    2022-11-17 14:17 - v1.5.8

--- src/Session.php
+++ src/Session.php
@@ -7,9 +7,11 @@ class Session {
 
   protected $Authentication = null;
   protected $Base64 = true;
+  protected $Timestamp = null;
 
   public function __construct(){
     $this->Authentication = $this->getSQLAuth();
+    $this->setCookieSettings();
   }
 
   public function __call($name, $arguments) {
@@ -27,17 +29,32 @@ class Session {
     return is_array($this->Authentication);
   }
 
+  protected function setCookieSettings(){
+    if(isset($_SESSION,$_SESSION['sessionID']) || isset($_COOKIE,$_COOKIE['sessionID'])){
+      if(isset($_POST,$_POST['cookiesAccept']) && !isset($_COOKIE['cookiesAccept'])){
+        if(isset($_POST['cookiesAccept'])){
+          setcookie( "cookiesAccept", true, $this->Timestamp + 86400 );
+          setcookie( "cookiesAcceptEssentials", true, $this->Timestamp + 86400 );
+        }
+        if(isset($_POST['cookiesAcceptPerformance'])){ setcookie( "cookiesAcceptPerformance", true, $this->Timestamp + 86400 ); }
+        if(isset($_POST['cookiesAcceptQuality'])){ setcookie( "cookiesAcceptQuality", true, $this->Timestamp + 86400 ); }
+        if(isset($_POST['cookiesAcceptPersonalisations'])){ setcookie( "cookiesAcceptPersonalisations", true, $this->Timestamp + 86400 ); }
+      }
+    }
+  }
+
   protected function getSQLAuth(){
+    $return = null;
     if(isset($_SESSION,$_SESSION['sessionID'])){
-      return [ "sessionID" => $_SESSION['sessionID'], "timestamp" => $_SESSION['timestamp'] ];
+      $return = [ "sessionID" => $_SESSION['sessionID'], "timestamp" => $_SESSION['timestamp'] ];
     } elseif(isset($_COOKIE,$_COOKIE['sessionID'],$_COOKIE['timestamp'])){
-      return [ "sessionID" => $_COOKIE['sessionID'], "timestamp" => $_COOKIE['timestamp'] ];
+      $return = [ "sessionID" => $_COOKIE['sessionID'], "timestamp" => $_COOKIE['timestamp'] ];
     } elseif(isset($_POST,$_POST['username'],$_POST['password'])){
       $return = [ "username" => $_POST['username'], "password" => $_POST['password'], "timestamp" => time() ];
       if(isset($_POST['remember'])){ $return['timestamp'] = time() + (86400 * 30); }
-      return $return;
     }
-    return null;
+    if($return != null){ $this->Timestamp = $return['timestamp']; } else { $this->Timestamp = time(); }
+    return $return;
   }
 
   protected function sendOutput($data, $httpHeaders=array()) {
