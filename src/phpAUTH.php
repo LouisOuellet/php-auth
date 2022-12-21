7531d239161c68bc339582d8a5e92eb4af04b36c
Louis Ouellet <louis_ouellet@hotmail.com>
2022-12-21 13:11:56 -0500

    2022-12-21 13:11 - v2.0.2

--- src/phpAUTH.php
+++ src/phpAUTH.php
@@ -327,7 +327,7 @@ class phpAUTH {
     }
   }
 
-  public function isConnected(){ return isset($_SESSION,$_SESSION['sessionID']); }
+  public function isConnected(){ return ($this->User == null); }
 
   protected function sendOutput($data, $httpHeaders=array()) {
     switch($this->OutputType){
